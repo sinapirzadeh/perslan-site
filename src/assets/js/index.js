0602241fb990a73313e794a816762335cb817344
sinapirzadeh <sinapirzadeh1@gmail.com>
2024-08-10 12:39:51 +0330

    first commit

---
+++ src/assets/js/index.js
@@ -0,0 +1,6 @@
+// menu bar hide show
+var menu_list = document.getElementById('meno_list')
+
+function menu() {
+    menu_list.style.display === 'block' ? menu_list.style.display = 'none' : menu_list.style.display = 'block';
+}
